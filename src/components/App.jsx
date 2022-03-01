3adc52377eba740ffb2193b7580a8a91548d14eb
Brian Sánchez <brian.sanchez@inmediatum.com>
2022-02-28 18:39:57 -0600

    extend tailwind's theme spacing using design guide values

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,11 @@ export const App = () => {
   return (
     <div className="bg-brand-01 h-full">
         <h1>Hello, Inmediatum</h1>
-        <div className='bg-brand-02 p-1'></div>
+        <div className='bg-brand-02 p-xxs'></div>
         <p className='text-text-inverse'>I'm a react application built with the bare minimum</p>
+        <div className='py-md bg-brand-03 relative animate-pulse'>
+          <div className='absolute bottom-0 left-0 py-xs w-12 bg-error'></div>
+        </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,8 +21,19 @@ module.exports = {
         'success': '#6FCF97',
         'error': '#FB6130',
         'warning': '#F2C94C'
+      },
+      spacing: {
+        xxs: '4px',
+        xs: '8px',
+        sm: '16px',
+        md: '24px',
+        lg: '32px',
+        xl: '40px',
+        xxl: '48px',
+        '3xl': '64px',
+        '4xl': '128px'
       }
-    },
+    }
   },
   plugins: [],
 }
