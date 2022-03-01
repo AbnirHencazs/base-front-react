9e1e9631fb09f8df06da204d2e701b228e1b8364
Brian Sánchez <brian.sanchez@inmediatum.com>
2022-02-28 18:02:58 -0600

    implement bootstrap with customization to the theme on variables file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,9 @@ import '../styles/global.scss'
 
 export const App = () => {
   return (
-    <div>
-        <h1>Hello, Inmediatum</h1>
-        <p>I'm a react application built with the bare minimum</p>
+    <div className='container bg-primary'>
+        <h1 className='py-4 text-secondary'>Hello, Inmediatum</h1>
+        <p className='text-text-01 fw-bold'>I'm a react application built with the bare minimum</p>
     </div>
   )
 }
