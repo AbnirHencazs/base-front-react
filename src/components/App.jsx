cffaca7f8d6ad2c67c20d05d0d68bc34c0227e71
Brian Sánchez <brian.sanchez@inmediatum.com>
2022-03-01 16:59:12 -0600

    basic implementation of useApi

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import useApi from '../hooks/useApi'
 
 export const App = () => {
+  const service = useApi()
+
+  const handleClick = () => {
+    service.get()
+    .then((response) => {
+      console.log("no prob", response)
+    })
+    .catch((err) => {
+      console.error("problem", err)
+    })
+  }
+
   return (
     <div>
         <h1>Hello, Inmediatum</h1>
         <p>I'm a react application built with the bare minimum</p>
+        <button onClick={() => handleClick()}>Make api call</button>
     </div>
   )
 }
